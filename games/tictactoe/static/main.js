7b860c4bceb73bf166b1105a7615c8ab74f242ee
SYNORK <bence@synork.xyz>
2025-03-20 20:28:21 +0100

    UPDATE

--- games/tictactoe/static/main.js
+++ games/tictactoe/static/main.js
@@ -24,14 +24,13 @@ function handleCellClick(index) {
 
     currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
     message.textContent = `It's ${currentPlayer}'s turn`;
-
 }
 
 function checkWin() {
     const winPatterns = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
-        [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
-        [0, 4, 8], [2, 4, 6]             // diagonals
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
     ];
 
     return winPatterns.some(pattern => {
@@ -47,4 +46,5 @@ function checkDraw() {
 cells.forEach((cell, index) => {
     cell.addEventListener('click', () => handleCellClick(index));
 });
+
 message.textContent = `It's ${currentPlayer}'s turn`;
