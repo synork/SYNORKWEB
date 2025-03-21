372db517c60eff6db12db7f41bd3c9db3fd99937
SYNORK <bence@synork.xyz>
2025-03-21 15:54:43 +0100

    Update main.js

--- games/tictactoe/static/main.js
+++ games/tictactoe/static/main.js
@@ -65,11 +65,20 @@ function botMove() {
 }
 
 function checkWin() {
-    // ... (A korábbi checkWin() függvény)
+    const winPatterns = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
+
+    return winPatterns.some(pattern => {
+        const [a, b, c] = pattern;
+        return gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c];
+    });
 }
 
 function checkDraw() {
-    // ... (A korábbi checkDraw() függvény)
+    return gameBoard.every(cell => cell !== '');
 }
 
 cells.forEach((cell, index) => {
